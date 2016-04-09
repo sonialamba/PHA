9a6f36d935963c0c72bdccdcc87a03ac35e9775a
Gurleen Kaur Lamba <gurleenlamba02@gmail.com>
2016-04-09 18:59:21 +0530

    4th almost

--- PHA/app/src/main/java/com/example/surinder/pha/AlarmReceiver.java
+++ PHA/app/src/main/java/com/example/surinder/pha/AlarmReceiver.java
@@ -1,23 +1,36 @@
 package com.example.surinder.pha;
 
-import android.content.BroadcastReceiver;
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.widget.Toast;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.support.v4.content.WakefulBroadcastReceiver;
 
-public class AlarmReceiver extends BroadcastReceiver {
+public class AlarmReceiver extends WakefulBroadcastReceiver {
 
-        @Override
-        public void onReceive(Context k1, Intent k2) {
-            // TODO Auto-generated method stub
-            Toast.makeText(k1, "Alarm received!", Toast.LENGTH_LONG).show();
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        //this will update the UI with message
+        AlarmActivity inst = AlarmActivity.instance();
+        inst.setAlarmText("Alarm! Wake up! Wake up!");
 
+        //this will sound the alarm tone
+        //this will sound the alarm once, if you wish to
+        //raise alarm in loop continuously then use MediaPlayer and setLooping(true)
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+
+        //this will send a notification message
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                AlarmService.class.getName());
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
     }
-
 }
--- PHA/app/src/main/java/com/example/surinder/pha/MainActivity.java
+++ PHA/app/src/main/java/com/example/surinder/pha/MainActivity.java
@@ -1,9 +1,6 @@
 package com.example.surinder.pha;
 
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.app.TimePickerDialog;
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -14,13 +11,8 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
-import android.widget.DatePicker;
 import android.widget.TextView;
 import android.widget.TimePicker;
-import android.widget.Toast;
-
-import java.util.Calendar;
-import java.util.GregorianCalendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,19 +37,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        textAlarmPrompt = (TextView) findViewById(R.id.setAlarm);
-
-        buttonstartSetDialog = (Button) findViewById(R.id.button4);
-        buttonstartSetDialog.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                textAlarmPrompt.setText("");
-                openTimePickerDialog(false);
-
-            }
-        });
-
 
     }
 
@@ -67,54 +46,13 @@ public class MainActivity extends AppCompatActivity {
         overridePendingTransition(R.anim.push_down_in,R.anim.push_down_out);
         startActivity(intent);
     }
-    private void openTimePickerDialog(boolean is24r) {
-        Calendar calendar = Calendar.getInstance();
 
-        timePickerDialog = new TimePickerDialog(MainActivity.this,
-                onTimeSetListener, calendar.get(Calendar.HOUR_OF_DAY),
-                calendar.get(Calendar.MINUTE), is24r);
-        timePickerDialog.setTitle("Set Alarm Time");
-
-        timePickerDialog.show();
-
+    public void setAlarm(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, AlarmActivity.class);
+        overridePendingTransition(R.anim.push_down_in,R.anim.push_down_out);
+        startActivity(intent);
     }
-
-    TimePickerDialog.OnTimeSetListener onTimeSetListener = new TimePickerDialog.OnTimeSetListener() {
-
-        @Override
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-
-            Calendar calNow = Calendar.getInstance();
-            Calendar calSet = (Calendar) calNow.clone();
-
-            calSet.set(Calendar.HOUR_OF_DAY, hourOfDay);
-            calSet.set(Calendar.MINUTE, minute);
-            calSet.set(Calendar.SECOND, 0);
-            calSet.set(Calendar.MILLISECOND, 0);
-
-            if (calSet.compareTo(calNow) <= 0) {
-                // Today Set time passed, count to tomorrow
-                calSet.add(Calendar.DATE, 1);
-            }
-
-            setAlarm(calSet);
-        }
-    };
-
-    private void setAlarm(Calendar targetCal) {
-
-        textAlarmPrompt.setText("\n\n***\n" + "Alarm is set "
-                + targetCal.getTime() + "\n" + "***\n");
-
-        Intent intent = new Intent(getBaseContext(), AlarmReceiver.class);
-        PendingIntent pendingIntent = PendingIntent.getBroadcast(getBaseContext(), RQS_1, intent, 0);
-        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-        alarmManager.set(AlarmManager.RTC_WAKEUP, targetCal.getTimeInMillis(),
-                pendingIntent);
-
-    }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
